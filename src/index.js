d6ca07254759ea672e3e70c115ce1916596485dc
zhurakovska <n.sinawka@gmail.com>
2023-07-11 19:00:30 +0300

    df

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import axios from "axios";
 // import Notiflix from 'notiflix';
-
+import makeGalleryCard from './gallery-card.hbs'
 import {Pixabay} from './pixabay-api'
+
+const listEl = document.querySelector('.gallery')
+const searchFormEl = document.querySelector('.search-form')
+const loadMoreBtnEl = document.querySelector('.btn-load-more')
 
 const pixabay = new Pixabay();
 
-pixabay.axiosPhotos().then(response => {
-    console.log(response.data.hits)
-  })
-  .catch(error => {
-    // console.log(error);
-});
+const onLoadMoreBtnClick = (event) => {
+    pixabay.page +=1
+
+     pixabay.axiosPhotos()
+    .then(response => {
+        const {hits, total, totalHits} = response.data
+        const galleryMarkup = makeGalleryCard(hits)
+        listEl.insertAdjacentHTML('beforeend', galleryMarkup)
+        console.log(total)
+            
+    }).catch(error => {
+        console.log(error);
+    });
+    // if(  === pixabay.page) {
+    //     loadMoreBtnEl.classList.add('is-hidden')
+    //     loadMoreBtnEl.removeEventListener('click', onLoadMoreBtnClick)
+    // }
+}
+
+const onSearchFormSubmit = (event) => {
+    event.preventDefault()
+    const searchQuery = event.currentTarget.elements.searchQuery.value
+    pixabay.searchQuery = searchQuery
+
+    pixabay.axiosPhotos()
+    .then(response => {
+        const {hits, total, totalHits} = response.data
+        const galleryMarkup = makeGalleryCard(hits)
+        listEl.innerHTML = galleryMarkup
+        loadMoreBtnEl.classList.remove('is-hidden')
+        loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick)
+        const perPage = (total/500) / 20
+        console.log(perPage)
+            
+    }).catch(error => {
+        console.log(error);
+    });
+        
+}
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit)
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -4,17 +4,20 @@ export class Pixabay {
     #BASE_URL = 'https://pixabay.com/api/'
     #API_KEY = '38181676-c389c3ce2b1eee7a286cf8f0e';
 
-    constructor() {}
+    constructor() {
+        this.searchQuery = ''
+        this.page = 1
+    }
 
     axiosPhotos(){
         const searchParams = {
             key: this.#API_KEY,
-            q: 'dog',
+            q: this.searchQuery,
             image_type: 'photo',
             orientation: 'horizontal',
             safesearch: true,
             per_page: 20,
-            page: 1
+            page: this.page
         };
 
         return axios.get(`${this.#BASE_URL}?`, { params: searchParams });
